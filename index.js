549dcb95f8ca215a98cc1efb52184d285077cfe4
reliut-g <reliutg@gmail.com>
2021-09-10 23:55:00 +0200

    jajaja

---
+++ index.js
@@ -0,0 +1,66 @@
+(() => {
+  function objectToCSS(obj) {
+    let kebab = function (str) {
+      return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+    };
+    let css = "";
+    for (let key in obj) {
+      css += kebab(key) + ":" + obj[key] + ";";
+    }
+    return css;
+  }
+
+  const styles = {
+    button: {
+      minWidth: "115px",
+      textAlign: "center",
+      backgroundColor: "#f5c518",
+      borderRadius: "6px",
+      display: "inline-block",
+      color: "#000000",
+      fontWeight: "bold",
+      padding: ".6em .7em",
+      position: " fixed",
+      zIndex: 9999,
+      top: "10px",
+      right: "10px",
+      textDecoration: "none",
+    },
+  };
+
+  // return original title
+  const movieTitle = document
+    .querySelector('[data-testid="hero-title-block__original-title"]')
+    .innerText.split(": ")
+    .slice(1)
+    .join();
+
+  const parsedTitle = encodeURIComponent(movieTitle);
+
+  const providers = [
+    {
+      name: "1337x",
+      url: `https://1337x.unblockninja.com/srch?search=${parsedTitle}`,
+    },
+    {
+      name: "RARBG",
+      url: `https://rarbg.to/torrents.php?search=${parsedTitle}`,
+    },
+    {
+      name: "serchinganx",
+      url: `https://vkygil.github.io/serchinganx/index.html?q=${parsedTitle}`,
+    },
+  ];
+
+  providers.forEach((provider, index) => {
+    const button = document.createElement("a");
+    button.href = provider.url;
+    button.target = "_blank";
+    button.innerText = provider.name;
+    button.style = objectToCSS({
+      ...styles.button,
+      top: Number(styles.button.top.replace("px", "")) + index * 40 + "px",
+    });
+    document.body.appendChild(button);
+  });
+})();
