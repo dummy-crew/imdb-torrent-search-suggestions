c513c7604d4cfbc630aea39dfbe9f9cf98a286c2
Eliut Gon <reliutg@gmail.com>
2021-09-13 21:42:59 +0200

    fix: execute

--- background.js
+++ background.js
@@ -1,127 +1,93 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ isEnabled: true });
-  chrome.storage.sync.set({
-    switchHideElemData: {
-      main: true,
-      Details: false,
-      DidYouKnow: false,
-      DynamicFeature_Episodes: false,
-      FAQ: false,
-      MoreLikeThis: false,
-      News: false,
-      Photos: false,
-      Storyline: false,
-      TechSpecs: false,
-      contribution: false,
-      "title-cast": false,
-      "videos-section": false,
-    },
-  });
+chrome.runtime.onInstalled.addListener((reason) => {
+  if (reason === chrome.runtime.OnInstalledReason.INSTALL) {
+    chrome.storage.sync.set({ isEnabled: true });
+    chrome.storage.sync.set({
+      switchHideElemData: {
+        main: true,
+        Details: false,
+        DidYouKnow: false,
+        DynamicFeature_Episodes: false,
+        FAQ: false,
+        MoreLikeThis: false,
+        News: false,
+        Photos: false,
+        Storyline: false,
+        TechSpecs: false,
+        contribution: false,
+        'title-cast': false,
+        'videos-section': false,
+      },
+    });
+  }
 });
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  debugger
-  if (request.message === "set_enable") {
+  if (request.message === 'set_enable') {
     chrome.storage.sync.set(
       {
         isEnabled: request.payload,
       },
       () => {
         if (chrome.runtime.lastError) {
-          sendResponse({ message: "error" });
+          sendResponse({ message: 'error' });
           return;
         }
-        sendResponse({ message: "success" });
-      }
+        sendResponse({ message: 'success' });
+      },
     );
   }
-  if (request.message === "switchHideElemData") {
+  if (request.message === 'switchHideElemData') {
     chrome.storage.sync.set(
       {
         switchHideElemData: request.payload,
       },
       () => {
         if (chrome.runtime.lastError) {
-          sendResponse({ message: "error" });
+          sendResponse({ message: 'error' });
           return;
         }
-        sendResponse({ message: "success" });
-      }
+        sendResponse({ message: 'success' });
+        handleHideElemData({ data: request.payload });
+      },
     );
   }
   return true;
 });
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (
-    changeInfo.status === "loading" &&
-    /^https:\/\/www\.imdb\.com\/title\//.test(tab.url)
-  ) {
-    chrome.storage.sync.get("isEnabled", (data) => {
+  if (changeInfo.status === 'loading' && /^https:\/\/www\.imdb\.com\/title\//.test(tab.url)) {
+    chrome.storage.sync.get('isEnabled', (data) => {
       if (data.isEnabled) {
         chrome.scripting
           .executeScript({
             target: { tabId },
-            files: ["./foreground.js"],
+            files: ['./foreground.js'],
           })
-          .then(() => console.log("Script executed"))
+          .then(() => console.log('Script executed'))
           .catch((error) => console.error(error));
       }
     });
-
-    chrome.storage.sync.get("switchHideElemData", (data) => {
-      // if hide section is enabled
-      if (data.switchHideElemData.main) {
-        chrome.scripting
-          .executeScript({
-            target: { tabId },
-            func: hideSomeElements,
-            args: [data.switchHideElemData],
-          })
-          .then(() => {
-            console.log('ajajaja')
-          })
-          .catch((error) => console.error(error));
-      }
-    });
+    chrome.storage.sync.get('switchHideElemData', handleHideElemData);
   }
 });
 
+function handleHideElemData(data) {
+  if (data.switchHideElemData.main) {
+    chrome.scripting
+      .executeScript({
+        target: { tabId },
+        func: handleElementToShow,
+        args: [data.switchHideElemData],
+      })
+      .catch((error) => console.error(error));
+  }
+}
 
-
-// run on val change without reload.. TODO
-function hideSomeElements(hideElemData) {
-  const existHTMLElement = (id) => document.querySelector(`[data-testid="${id}"]`)
-  Object.keys(hideElemData).forEach((key) => {
-    if (key !== "main" && existHTMLElement(key)) {
-      if (hideElemData[key]) {
-        document.querySelectorAll(`[data-testid="${key}"]`)[0].style.display =
-          "none";
-        console.log("g63");
-      } else {
-        document.querySelectorAll(`[data-testid="${key}"]`)[0].style.display =
-          "block";
-        console.log("g64");
-      }
+function handleElementToShow(elements) {
+  const existElement = (key) => document.querySelector(`[data-testid="${key}"]`);
+  Object.keys(elements).forEach((key) => {
+    if (existElement(key)) {
+      document.querySelector(`[data-testid="${key}"]`).style.display = !elements[key] ? 'none' : 'block';
     }
   });
 }
-
-// chrome.storage.sync.set({ isEnabled: true });
-// chrome.storage.sync.set({
-//   switchHideElemData: {
-//     main: true,
-//     Details: false,
-//     DidYouKnow: false,
-//     DynamicFeature_Episodes: false,
-//     FAQ: false,
-//     MoreLikeThis: false,
-//     News: false,
-//     Photos: false,
-//     Storyline: false,
-//     TechSpecs: false,
-//     contribution: false,
-//     "title-cast": false,
-//     "videos-section": false,
-//   },
-// });
--- popup-script.js
+++ popup-script.js
@@ -1,6 +1,6 @@
-const switchToggle = document.getElementById("mainSwitch");
-const switchHideElem = document.getElementById("switchHideElem");
-const hideElemList = document.getElementById("hideElemList");
+const switchToggle = document.getElementById('mainSwitch');
+const switchHideElem = document.getElementById('switchHideElem');
+const hideElemList = document.getElementById('hideElemList');
 
 //Un commnent for clearing data real quick
 // chrome.storage.sync.clear(function (obj) {
@@ -10,101 +10,83 @@ const hideElemList = document.getElementById("hideElemList");
 let switchHideElemData = {};
 
 const hideElemListArr = [
-  ["DidYouKnow", "Did you Know"],
-  ["Storyline", "Story Line"],
-  ["MoreLikeThis", "More Like This"],
-  ["videos-section", "videos section"],
-  ["Photos", "Photos"],
-  ["DynamicFeature_Episodes", "Epidodes"],
-  ["title-cast", "Top Cast"],
-  ["FAQ", "FAQ"],
-  ["Details", "Details"],
-  ["TechSpecs", "Technical Specifications"],
-  ["News", "Related news"],
-  ["contribution", "Contribute to this page"],
+  ['DidYouKnow', 'Did you Know'],
+  ['Storyline', 'Story Line'],
+  ['MoreLikeThis', 'More Like This'],
+  ['videos-section', 'videos section'],
+  ['Photos', 'Photos'],
+  ['DynamicFeature_Episodes', 'Epidodes'],
+  ['title-cast', 'Top Cast'],
+  ['FAQ', 'FAQ'],
+  ['Details', 'Details'],
+  ['TechSpecs', 'Technical Specifications'],
+  ['News', 'Related news'],
+  ['contribution', 'Contribute to this page'],
 ];
 
-let renderHideElemListArr = () => {
-  let html = "";
-  hideElemListArr.forEach(function (el) {
-    html += `
+let html = '';
+hideElemListArr.forEach(function (el) {
+  html += `
     <label class="container">Hide ${el[1]}
       <input class="hideElemListCB" data-testid="${el[0]}" type="checkbox">
       <span class="checkmark"  ></span>
     </label>
     `;
-  });
-  hideElemList.innerHTML = html;
-};
-
-renderHideElemListArr();
-
-// setup listener for every checkbox
-Array.from(document.getElementsByClassName("hideElemListCB")).forEach(function (
-  element
-) {
-  element.addEventListener("change", (event) => {
-    console.log(event.currentTarget.checked);
-    switchHideElemData[event.currentTarget.getAttribute("data-testid")] =
-      event.currentTarget.checked;
-      handleSwitchHideElemData();
-  });
 });
 
-chrome.storage.sync.get("isEnabled", (data) => {
+hideElemList.innerHTML = html;
+
+// setup listener for every checkbox
+Array.from(hideElemList.querySelectorAll('.hideElemListCB')).forEach(function (element) {
+  element.addEventListener('change', handleSwitchHideElemData);
+});
+
+// Setup initial state
+chrome.storage.sync.get('isEnabled', (data) => {
   switchToggle.checked = data.isEnabled;
 });
 
-chrome.storage.sync.get("switchHideElemData", (data) => {
+chrome.storage.sync.get('switchHideElemData', (data) => {
   switchHideElemData = data.switchHideElemData;
   if (data.switchHideElemData) {
     switchHideElem.checked = data.switchHideElemData.main;
-  }
-  else switchHideElemData = {};
+  } else switchHideElemData = {};
   Object.keys(switchHideElemData).forEach((key) => {
-    if (key !== "main") {
+    if (key !== 'main') {
       document.querySelector(`[data-testid="${key}"]`).checked = switchHideElemData[key];
     }
   });
 });
 
-//main Switch
-switchToggle.addEventListener("change", (e) => {
+// Extension switcher
+switchToggle.addEventListener('change', (e) => {
   chrome.runtime.sendMessage(
     {
-      message: "set_enable",
+      message: 'set_enable',
       payload: e.target.checked,
     },
     (response) => {
-      if (response.message === "success") {
-        console.log("Successfully set enable to " + e.target.checked);
-      } else {
-        console.log(
-          "Failed to set enable to " + e.target.checked + " " + response.message
-        );
+      if (response.message !== 'success') {
+        console.error('Failed to set enable to ' + e.target.checked + ' ' + response.message);
       }
-    }
+    },
   );
 });
 
-//Main "Hide some elements" Switch
-switchHideElem.addEventListener("change", (e) => {
-  switchHideElemData["main"] = e.target.checked;
-  handleSwitchHideElemData();
-});
+// Hide section switcher
+switchHideElem.addEventListener('change', handleSwitchHideElemData);
 
-function handleSwitchHideElemData() {
+function handleSwitchHideElemData(e) {
+  switchHideElemData[e.target.dataset['testid']] = e.target.checked;
   chrome.runtime.sendMessage(
     {
-      message: "switchHideElemData",
+      message: 'switchHideElemData',
       payload: switchHideElemData,
     },
     (response) => {
-      if (response.message === "success") {
-        console.log("Successfully set setSwitchHideElem");
-      } else {
-        console.log("Failed to set enable to setSwitchHideElem");
+      if (response.message !== 'success') {
+        console.error('Failed handleSwitchHideElemData');
       }
-    }
+    },
   );
 }
